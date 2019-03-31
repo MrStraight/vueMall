f249868265276e1d67077900847730fd19d89e07
lih <542356155@qq.com>
2019-03-31 13:34:07 +0800

    Home_Header+Nav完成

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,16 +1,18 @@
 <template>
 	<div>
 		<home-header></home-header>
+		<home-nav></home-nav>
 	</div>
 </template>
 
 <script>
 	import HomeHeader from './components/header'
+	import HomeNav from './components/HomeNav'
 export default {
 	name: 'Home',
 	components:{
 		HomeHeader,
-	
+		HomeNav
 	}
 }
 </script>
