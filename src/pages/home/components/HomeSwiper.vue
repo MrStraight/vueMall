8b90cd69409d4e35541fa9b2e5f4af954f441378
lih <542356155@qq.com>
2019-04-01 14:04:21 +0800

    HomeSet完成

--- src/pages/home/components/HomeSwiper.vue
+++ src/pages/home/components/HomeSwiper.vue
@@ -2,51 +2,57 @@
 	<div class="HomeSwiper">
 		<swiper :options="swiperOption">
 			<!-- slides -->
-			<swiper-slide><div class="img"><img src="../../../assets/imgs/banner2.jpg"></div></swiper-slide>
-			<swiper-slide><div class="img"><img src="../../../assets/imgs/banner1.jpg"></div></swiper-slide>
-			<swiper-slide><div class="img"><img src="../../../assets/imgs/banner3.jpg"></div></swiper-slide>
-			<swiper-slide><div class="img"><img src="../../../assets/imgs/banner4.jpg"></div></swiper-slide>
+			<swiper-slide>
+				<div class="img"><img src="../../../assets/imgs/banner2.jpg" /></div>
+			</swiper-slide>
+			<swiper-slide>
+				<div class="img"><img src="../../../assets/imgs/banner1.jpg" /></div>
+			</swiper-slide>
+			<swiper-slide>
+				<div class="img"><img src="../../../assets/imgs/banner3.jpg" /></div>
+			</swiper-slide>
+			<swiper-slide>
+				<div class="img"><img src="../../../assets/imgs/banner4.jpg" /></div>
+			</swiper-slide>
 			<!-- Optional controls -->
-			<div class="swiper-pagination"  slot="pagination"></div>
+			<div class="swiper-pagination" slot="pagination"></div>
+			
 		</swiper>
 	</div>
 </template>
 
 <script>
- export default {
-    name: 'HomeSwiper',
-    data() {
-      return {
-        swiperOption: {
-			pagination:'.swiper-pagination',
-			loop:true,
-			autoplay:3000,
-			
-        },
-		swiperList:{
-			id:'001',
-			imgUrl:''
-		}
-      }
-    }
-  }
-  
+export default {
+	name: 'HomeSwiper',
+	data() {
+		return {
+			swiperOption: {
+				pagination: '.swiper-pagination',
+				loop: true,
+				autoplay: 3000
+			},
+			swiperList: {
+				id: '001',
+				imgUrl: ''
+			}
+		};
+	}
+};
 </script>
 
 <style scoped="scoped">
-	.img{
-		width: 100%;
-		margin-top: 0.1875rem;
-	}
-	.img img{
-		width: 100%;
-	}
-	
+.img {
+	width: 100%;
+	margin-top: 0.1875rem;
+}
+.img img {
+	width: 100%;
+}
 </style>
 <style>
-	.swiper-pagination-bullet{
-		width: 1.6rem !important;
-		height: 0.25rem !important;
-		border-radius: 15%;
-	}
+.swiper-pagination-bullet {
+	width: 1.6rem !important;
+	height: 0.25rem !important;
+	border-radius: 15%;
+}
 </style>
--- src/pages/home/components/set.vue
+++ src/pages/home/components/set.vue
@@ -1,16 +1,159 @@
 <template>
 	<div class="set">
 		<div class="title">新品首发 <span class="iconfont icon-xiayiye"></span></div>
-	</div>
+		<ul class="title2">
+			<li @click="change">客厅</li>
+			<li @click="change2">卧室</li>
+			<li @click="change3">餐厅</li>
+			<li @click="change4">书房</li>
+		</ul>
+		<swiper class="swiper-wrapper" ref="mySwiper" :options="swiperOption">
+			<swiper-slide class="swiper-container">
+				<div class="img"><img src="@/assets/imgs/set1.jpg"></div>
+				<ul class="smallImg">
+					<li>
+						<img src="@/assets/imgs/set1-1.jpg">
+						<p class="info">白椿木框架沙发</p>
+						<p class="price">￥<span>9998</span></p>
+					</li>
+					<li>
+						<img src="@/assets/imgs/set1-2.jpg">
+						<p class="info">白椿木框架沙发</p>
+						<p class="price">￥<span>9998</span></p>
+					</li>
+					<li>
+						<img src="@/assets/imgs/set1-3.jpg">
+						<p class="info">白椿木框架沙发</p>
+						<p class="price">￥<span>9998</span></p>
+					</li>
+				</ul>
+			</swiper-slide>
+			<swiper-slide class="swiper-container">
+				<div class="img"><img src="@/assets/imgs/set2.jpg"></div>
+				<ul class="smallImg">
+					<li>
+						<img src="@/assets/imgs/set2-1.jpg">
+						<p class="info">白椿木框架沙发</p>
+						<p class="price">￥<span>9998</span></p>
+					</li>
+					<li>
+						<img src="@/assets/imgs/set2-2.jpg">
+						<p class="info">白椿木框架沙发</p>
+						<p class="price">￥<span>9998</span></p>
+					</li>
+					<li>
+						<img src="@/assets/imgs/set2-3.jpg">
+						<p class="info">白椿木框架沙发</p>
+						<p class="price">￥<span>9998</span></p>
+					</li>
+				</ul>
+			</swiper-slide>
+			<swiper-slide class="swiper-container">
+				<div class="img"><img src="@/assets/imgs/set3.jpg"></div>
+				<ul class="smallImg">
+					<li>
+						<img src="@/assets/imgs/set3-1.jpg">
+						<p class="info">白椿木框架沙发</p>
+						<p class="price">￥<span>9998</span></p>
+					</li>
+					<li>
+						<img src="@/assets/imgs/set3-2.jpg">
+						<p class="info">白椿木框架沙发</p>
+						<p class="price">￥<span>9998</span></p>
+					</li>
+					<li>
+						<img src="@/assets/imgs/set3-3.jpg">
+						<p class="info">白椿木框架沙发</p>
+						<p class="price">￥<span>9998</span></p>
+					</li>
+				</ul>
+			</swiper-slide>
+			<swiper-slide class="swiper-container">
+				<div class="img"><img src="@/assets/imgs/set4.jpg"></div>
+				<ul class="smallImg">
+					<li>
+						<img src="@/assets/imgs/set4-1.jpg">
+						<p class="info">白椿木框架沙发</p>
+						<p class="price">￥<span>9998</span></p>
+					</li>
+					<li>
+						<img src="@/assets/imgs/set4-2.jpg">
+						<p class="info">白椿木框架沙发</p>
+						<p class="price">￥<span>9998</span></p>
+					</li>
+					<li>
+						<img src="@/assets/imgs/set4-3.jpg">
+						<p class="info">白椿木框架沙发</p>
+						<p class="price">￥<span>9998</span></p>
+					</li>
+				</ul>
+			</swiper-slide>
+			<div class="swiper-pagination" slot="pagination">
+			</div>
+		</swiper>
+	</div> 
+
 </template>
 
 <script>
+	
 	export default{
-		name:'set'
+		name:'set',
+		data(){
+			return{
+				showBorder1:true,
+				showBorder2:false,
+				showBorder3:false,
+				showBorder4:false,
+				swiperOption:{
+					atuo:false,
+					loop:false,
+				}
+			}
+		},
+		computed: {
+		  swiper() {
+			return this.$refs.mySwiper.swiper
+		  }
+		},
+		mounted() {
+				
+		},
+		methods:{
+			change(){
+				var a = document.getElementsByTagName('li')
+				this.swiper.slideTo(0, 500, false);//切换到第一个slide，速度为0.5秒
+				
+			},
+			change2(){
+				this.swiper.slideTo(1, 1000, false);
+				console.log(this.swiper.realIndex);
+				this.showBorder = true
+			},
+			change3(){
+				this.swiper.slideTo(2, 1000, false);
+				console.log(this.swiper.realIndex)
+			},
+			change4(){
+				this.swiper.slideTo(3, 1000, false);
+				console.log(this.swiper.realIndex)
+			},
+		}
 	}
+	
 </script>
 
 <style scoped="scoped">
+	img{
+		width: 92%;
+	}
+	.smallImg img{
+		width: 95%;
+	}
+	.smallImg li{
+		width: 31%;
+		text-align: center;
+	}
 	.title{
 		font-size: 1.25rem;
 		font-weight: 700;
@@ -19,5 +162,36 @@
 	}
 	.title span{
 		font-size: 1.125rem;
+	}
+	ul{
+		display: flex;
+		flex-grow: 1;
+		justify-content: center;
+	}
+	.title2 li{
+		margin: 0 auto;
+	}
+	.img{
+		margin: 1rem auto .5rem;
+		text-align: center;
+	}
+	.info,.price{
+		text-align: left;
+		margin-left: 0.3125rem;
+	}
+	.info{
+		font-size: 0.9rem;
+		color: #444;
+		margin-top: 0.1875rem;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+	.price{
+		margin-top: 0.4rem;
+	}
+	.price,.price span{
+		font-size: .85rem;
+		color: #c81c28;
 	}
 </style>
